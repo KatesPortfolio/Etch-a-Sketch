96e85696750e69243c4954ab5da395a0bee54d7a
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-07-08 18:59:44 +0200

    Function: createGrid
    FIRST PART:
    
    To create the grid i iterated 16 times to create all the divs.
    
    The hardest part was how I was going to add the colors and remove them. I knew that I had to use "add" and "remove" but then I remembered one time using "contains" which made it work.
    
    Lesson:
    - I needed to use 2 mouseover. One for making the divs change color, and one for "getting" them back to original color on the way back.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const container = document.querySelector(".container");
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const div = document.createElement("div");
+      div.style.border = "1px solid white";
+      container.appendChild(div);
+      div.addEventListener("mouseover", () => {
+        if (div.classList.add("addColor")) {
+          div.classList.remove("removeColor");
+        }
+
+        div.addEventListener("mouseover", () => {
+          if (div.classList.contains("addColor")) {
+            div.classList.add("removeColor");
+          }
+        });
+      });
+    }
+  }
+}
+createGrid();
